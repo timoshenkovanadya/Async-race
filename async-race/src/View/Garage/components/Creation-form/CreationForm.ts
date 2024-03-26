b05709e8b934354f1002ac6bd6084bad5318a2b6
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2024-03-26 10:21:20 +0300

    feat: add car-creation form on the garage-page

---
+++ async-race/src/View/Garage/components/Creation-form/CreationForm.ts
@@ -0,0 +1,77 @@
+
+export class CreationForm {
+    private formContainer: HTMLElement;
+
+    private createContainer: HTMLElement;
+
+    private updContainer: HTMLElement;
+
+    private createInput: HTMLInputElement;
+
+    private updInput: HTMLInputElement;
+
+    private createButton: HTMLButtonElement;
+
+    private updButton: HTMLButtonElement;
+
+    private createColorInput: HTMLInputElement;
+
+    private updColorInput: HTMLInputElement;
+
+    constructor () {
+        this.formContainer = document.createElement('div');
+        this.formContainer.className = 'form-container';
+        this.createContainer = document.createElement('form');
+        this.createContainer.className = 'create-container';
+        this.formContainer.append(this.createContainer);
+         
+        this.updContainer = document.createElement('form');
+        this.updContainer.className = 'create-container';
+        this.formContainer.append(this.updContainer);
+
+
+        this.createInput = this.addInput('Create', this.createContainer);
+        this.updInput = this.addInput('Update', this.updContainer);
+
+        this.createColorInput = this.addColorInput('create', this.createContainer);
+        this.updColorInput = this.addColorInput('update', this.updContainer);
+
+        this.createButton = this.addButton('create', this.createContainer);
+        this.updButton = this.addButton('update', this.updContainer);
+    }
+
+    addInput(inputName: string, parent: HTMLElement): HTMLInputElement {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'text-input';
+        input.name = inputName;
+        parent.appendChild(input);
+
+        return input;
+    }
+
+    addButton(text: string, parent: HTMLElement): HTMLButtonElement {
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.className = 'create-button';
+        button.textContent = text;
+        parent.appendChild(button);
+        return button;
+    }
+
+    addColorInput(inputName: string, parent: HTMLElement): HTMLInputElement {
+        const input = document.createElement('input');
+        input.type = 'color';
+        input.className = 'color-input';
+        input.name = inputName;
+        parent.appendChild(input);
+
+        return input;
+    }
+
+    renderTo(parent: HTMLElement): void {
+        parent.appendChild(this.formContainer);
+    }
+
+
+}
